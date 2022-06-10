8f103b2a7f49a8498b187fcda318d4cb432a2664
hernandezs2000 <hernandezs2000nic@gmail.com>
2022-06-10 08:40:57 +0800

    new edit

--- user/profile.php
+++ user/profile.php
@@ -70,7 +70,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
 	    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Time-in Record</title>
+        <title>Profile Information</title>
         <link rel="stylesheet" type="text/css" href="h21style.css">
     </head>
         <body>
