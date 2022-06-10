ea8cae9b3be5f4bef14a5794b59fd34d477537ae
hernandezs2000 <hernandezs2000nic@gmail.com>
2022-06-10 08:41:03 +0800

    new edit

--- entryrecord/entry.php
+++ entryrecord/entry.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE-edge">
 	    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Home Page</title>
+        <title>Time-in Record</title>
         <script src="clock.js"></script>
         <link rel="stylesheet" type="text/css" href="h1style.css">
     </head>
